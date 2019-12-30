28b84269e3340ff626609611a4c95504c4cf7d2a
Surfsol <surfsol@protonamil.com>
2019-12-30 17:49:33 -0300

    npm i twilio, Add twilio-router and put in server

--- api/server.js
+++ api/server.js
@@ -1,26 +1,28 @@
-const express = require('express')
-const server = express()  //create instance of express server
-const CORS = require('cors')
-const helmet = require('helmet')
+const express = require("express");
+const server = express(); //create instance of express server
+const CORS = require("cors");
+const helmet = require("helmet");
 
+const proRouter = require("../routes-models/projects/project-routes");
+const tecRouter = require("../routes-models/techs/techs-router");
+const techProjRouter = require("../routes-models/techproj/techInProj-router");
+const ptRouter = require("../routes-models/pt/pt-router");
+const techsProjectRouter = require("../routes-models/techsProject/techsProject-router");
+const twilioRouter = require("../routes-models/Twilio/twilio-router");
 
-const proRouter = require('../routes-models/projects/project-routes')
-const tecRouter = require('../routes-models/techs/techs-router')
-const techProjRouter = require('../routes-models/techproj/techInProj-router')
-const ptRouter = require('../routes-models/pt/pt-router')
-const techsProjectRouter = require('../routes-models/techsProject/techsProject-router')
+server.use(express.json()); // allows express to read .json from body of request
+server.use(CORS());
+server.use(helmet());
 
-server.use(express.json())// allows express to read .json from body of request
-server.use(CORS())
-server.use(helmet())
+server.use("/projects", proRouter);
+server.use("/tech", tecRouter);
+server.use("/techInProject", techProjRouter);
+server.use("/pt", ptRouter);
+server.use("/techsProject", techsProjectRouter);
+server.use("/twilio", twilioRouter);
 
-
-
-server.use('/projects', proRouter)
-server.use('/tech', tecRouter)
-server.use('/techInProject', techProjRouter)
-server.use('/pt', ptRouter)
-server.use('/techsProject', techsProjectRouter)
-server.get('/', (req, res) => { res.status(200).json({hello: process.env.MOTD})})
+server.get("/", (req, res) => {
+  res.status(200).json({ hello: process.env.MOTD });
+});
 
 module.exports = server;
---
+++ routes-models/Twilio/twilio-router.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const twilio = require("twilio");
+
+const accountSid = process.env.TWILIOID;
+const authToken = process.env.AUTHTWILIO;
+const client = new twilio(accountSid, authToken);
+
+
+router.get("/", (req, res) => {
+
+    res.send('Hello to Twilio')
+    const { recipient, textmessage } = req.body;
+    console.log(recipient)
+    console.log(textmessage)
+    
+    //send text
+    client.messages
+      .create({
+        body: textmessage,
+        to: recipient,
+        from: "+12284714144"
+      })
+      .then(message => console.log(message.body));
+  });
+
+module.exports = router;
